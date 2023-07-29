76805146ce0f523aa5ebcd1f9ef8120be90b07ac
frank <sunwu51@126.com>
2023-07-29 22:10:19 +0800

    add a little error catch

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config()// 加载当前目录下的.env文件作为环境变量
+
 const express = require('express');
 const conversationRouter = require('./conversation.js');
 const chat =  require('./chat.js');
-const dotenv = require('dotenv');
-dotenv.config() // 加载当前目录下的.env文件作为环境变量
 
 const app = express();
 app.use(express.static('dist'));
